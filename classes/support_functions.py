1f64da0e91a2b21066102b1adada2ba90d124b47
DanIrl <danjohansson98@gmail.com>
2024-02-20 10:08:54 +0100

    Created function to download images

--- classes/support_functions.py
+++ classes/support_functions.py
@@ -1,5 +1,6 @@
 import librosa
 import soundfile as sf
+import requests
 
 
 def change_pitch_without_affecting_speed(audio_file_path, n_steps):
@@ -12,3 +13,12 @@ def change_pitch_without_affecting_speed(audio_file_path, n_steps):
     # Save the modified audio
     audio_file_path = audio_file_path[:-4] + "_shifted.mp3"
     sf.write(audio_file_path, y_shifted, sr)
+
+
+def download_images(image_urls: list[str], save_path: str):
+    """Provided json  file with openAI image links, download the images to the specified path"""
+
+    for i, url in enumerate(image_urls):
+        response = requests.get(url)
+        with open(f"{save_path}/image_{i}.png", "wb") as file:
+            file.write(response.content)
